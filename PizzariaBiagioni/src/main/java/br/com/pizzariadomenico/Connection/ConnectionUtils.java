3d309cb64236d6ddb8aa761860fc3d442ec20543
jonatahessa <jonata.hessa@gmail.com>
2017-12-11 19:03:05 -0200

    dados

--- PizzariaBiagioni/src/main/java/br/com/pizzariadomenico/Connection/ConnectionUtils.java
+++ PizzariaBiagioni/src/main/java/br/com/pizzariadomenico/Connection/ConnectionUtils.java
@@ -25,8 +25,8 @@ public class ConnectionUtils {
             String serveName = "bancobiagioni1.mysql.uhserver.com"; // Caminho do servidor do BD
             String mydatabase = "bancobiagioni1"; // Nome do banco de dados
             String url = "jdbc:mysql://" + serveName + "/" + mydatabase;
-            String username = "biagioni01"; // Nome de usuario
-            String password = "pizz@ri@2017"; // Senha de acesso
+            String username = ""; // Nome de usuario
+            String password = ""; // Senha de acesso
             connection = DriverManager.getConnection(url, username, password);
             
             //Teste de conexao
